dfbb522638750bdda1d3ae56a8c311df7f0ca05b
Vanessa Bianconi <79461531+vanessabianconi@users.noreply.github.com>
2023-02-25 11:52:43 +0100

    Delete resources directory

--- resources/js/postillemostra.js
+++
@@ -1,6 +0,0 @@
-
-     $(document).ready(function() {
-        for (var i = 0; i < $('.linkpost').length; i++) {
-           $('.linkpost:eq(' + i + ')').text('Postilla ' + i);
-        };
-     });
